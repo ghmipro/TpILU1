04ef359b313e412df212e3ad532b2b3c97e02d51
Mohamed islam Ghaffar <ghm3061a@ensfsi.onmicrosoft.com>
2024-09-19 11:51:28 +0200

    coder rapidement

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,41 @@
 package personnages;
 
 public class Gaulois {
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public static void main(String[] args) {
+		
+	}
 
 }
