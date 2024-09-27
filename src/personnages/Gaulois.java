ebe93e0b9092625e64e2918b26a7825d705fdd41
Mohamed islam Ghaffar <ghm3061a@ensfsi.onmicrosoft.com>
2024-09-27 09:42:58 +0200

    je suis presque a la fin

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -25,17 +25,38 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
 
 	public void setEffetPotion(int effetPotion) {
 		this.effetPotion = effetPotion;
 	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+		+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
 
 	@Override
-	public String toString() {
+   public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
+		}
+	
+	
 	public static void main(String[] args) {
-		
-	}
-
+        
+        Gaulois asterix = new Gaulois("Astérix", 8);
+        System.out.println(asterix);
+        System.out.println(asterix.prendreParole());
+        asterix.parler(""); 
+        asterix.frapper();
+        }
 }
