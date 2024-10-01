b1528152bf80597e883e234a08028c2311527d2a
Mohamed islam Ghaffar <ghm3061a@ensfsi.onmicrosoft.com>
2024-10-01 09:40:12 +0200

    j'ai finis le Tp1 debut du Tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,18 +1,36 @@
 package personnages;
 
-public class Village{
-	private String nom;
-	private Chef chef;
-	public Village(String nom) {
-		this.nom = nom;
-	}
-	public void setChef(Chef chef) {
-		this.chef = chef;
-		}
-	public String getNom() {
-		return nom;
-	}
+public class Village {
+    private String nom;
+    private Chef chef;
+    private Gaulois[] villageois; 
+    private int nbVillageois; 
+
+    public Village(String nom, int nbVillageoisMaximum) {
+        this.nom = nom;
+        this.villageois = new Gaulois[nbVillageoisMaximum]; 
+        this.nbVillageois = 0; 
+    }
+
+    public void setChef(Chef chef) {
+        this.chef = chef;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void ajouterVillageois(Gaulois villageois) {
+        this.villageois[nbVillageois] = villageois;
+        nbVillageois++;
+    }
+
+    public int getNbVillageois() {
+        return nbVillageois;
+    }
 }
+
+
 
 
 
